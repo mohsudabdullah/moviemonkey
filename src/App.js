d4eb08c740f7d6b57cccf3ee9a2d454350975fd7
mohsudabdullah <mohsudabdullah@gmail.com>
2019-05-27 19:33:32 +0300

    data fetch testing is working

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
 import React from 'react';
+import axios from 'axios';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default class App extends React.Component  {
+  constructor()
+    {
+        super();
+        this.state={
+            shop:{},
+            users:[],
+            filtered:[],
+            loading:true,
+            searchValue:''
+        };
+        this.handleSearchChange = (searchValue) => {
+            this.setState({searchValue});
+            this.filter();
+          };
+    }
+
+    componentDidMount()
+        {
+            axios.get('https://jsonplaceholder.typicode.com/todos/1')
+            .then(response=>{
+                this.state.loading=false
+                this.setState({users:response.data})
+            });
+  
+            console.log(this.state.users)
+
+        }
+  render() {
+
   return (
     <div className="App">
       <header className="App-header">
@@ -22,5 +53,5 @@ function App() {
     </div>
   );
 }
+}
 
-export default App;
